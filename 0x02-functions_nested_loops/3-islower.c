5bf31b73f1f4f00f828b62567a8510084d9c37df
Olamide <olire96@gmail.com>
2022-09-13 15:56:44 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+  * _islower - checks if a character is lower case
+  *
+  * Return: It returns 1 if it's a lower case and zero if it isn't. 
+  */
+
+int _islower(int c);
+{
+	int checker = islower(c);i
+	
+	if (checker > 0):
+		return(1);
+	else if (checker == 0):
+		return(0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,39 @@
-#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+int _islower(int c);
 
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
+	int r;
+
+    	r = _islower('H');
+    	putchar(r + '0');
+    	r = _islower('o');
+    	putchar(r + '0');
+    	r = _islower(108);
+    	putchar(r + '0');
+    	putchar('\n');
+    	return (0);    
+}
+
+/**
+  * _islower - checks if a character is lower case
+  *
+  * Return: It returns 1 if it's a lower case and zero if it isn't. 
+  */
+
+int _islower(int c)
+{
+	int checker = islower(c);
+	
+	if (checker > 0)
+		return(1);
+	else if (checker == 0)
+		return(0);
 }
