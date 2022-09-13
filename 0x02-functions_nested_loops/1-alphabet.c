9e66162939d841dc20d2fdf654c51c7aeee96b63
Olamide <olire96@gmail.com>
2022-09-13 15:14:23 +0000

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+	int alphabet = 97;
+	int i;
+	/*Create a for loop with 25*/
+	for (i = 0; i < 26; i++)
+		putchar(alphabet + i);
+	putchar(10);
+	/*Use putc in the for loop*/
+}
