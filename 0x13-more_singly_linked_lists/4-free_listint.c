25ea21fe77821ba5df673700606ba533614bfa42
Olamide <olire96@gmail.com>
2022-10-26 04:53:19 +0100

    check recursion

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <string.h>
 
 /**
  * free_listint - frees a list
@@ -7,12 +6,7 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *traverse;
-
-	while (head)
-	{
-		traverse = head->next;
-		free(head);
-		head = traverse;
-	}
+	if (head->next != NULL)
+		free_listint(head->next);
+	free (head);
 }
