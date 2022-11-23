65206ef2267878ed15856739ffa42805b56f1476
Olamide <olire96@gmail.com>
2022-11-23 18:28:54 +0100

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print prime factor of a number
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int factor = 50829599;
+
+	printf("%i\n", factor);
+	return (0);
+}
